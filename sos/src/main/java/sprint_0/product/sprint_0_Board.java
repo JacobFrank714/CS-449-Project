94c86fdef1a1873ff5e17c927f41bc081c43bd7a
mcfreak12 <jacobfrank714@gmail.com>
2022-09-08 12:02:08 -0500

    basic squares with no actions

---
+++ sos/src/main/java/sprint_0/product/sprint_0_Board.java
@@ -0,0 +1,18 @@
+package sprint_0.product;
+
+public class sprint_0_Board {
+    private int[][] grid;
+	private char turn = 'X';
+
+	public sprint_0_Board(int x) {
+		grid = new int[x][x];
+	}
+
+	public int getCell(int row, int column) {
+		return grid[row][column];
+	}
+
+	public char getTurn() {
+		return turn;
+	}
+}
--- sos/src/main/java/sprint_0/product/sprint_0_GUI.java
+++ sos/src/main/java/sprint_0/product/sprint_0_GUI.java
@@ -1,38 +1,84 @@
 package sprint_0.product;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+// import javafx.fxml.FXML;
+// import javafx.fxml.FXMLLoader;
+// import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
-import java.io.IOException;
+// import java.io.IOException;
 
 public class sprint_0_GUI extends Application{
-    
-    private static Scene scene;
+
+    private Square[][] squares;
+
+    private Label gameStatus = new Label("Player 1's Turn");
+
+    static private sprint_0_Board board;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage primaryStage) {
+		if (board == null) {
+			board = new sprint_0_Board(8);
+		}
+		GridPane pane = new GridPane();
+		squares = new Square[8][8];
+		for (int i = 0; i < 8; i++)
+			for (int j = 0; j < 8; j++)
+				pane.add(squares[i][j] = new Square(), j, i);
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(pane);
+		borderPane.setBottom(gameStatus);
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(sprint_0_GUI.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+		Scene scene = new Scene(borderPane, 700, 700);
+		primaryStage.setTitle("SOS Game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        sprint_0_GUI.setRoot("secondary");
-    }
+    public class Square extends Pane {
+		public Square() {
+			setStyle("-fx-border-color: white");
+			this.setPrefSize(2000, 2000);
+			this.setOnMouseClicked(e -> handleMouseClick());
+		}
+
+		private void handleMouseClick() {
+		}
+
+	}
+
+    // static void setRoot(String fxml) throws IOException {
+    //     scene.setRoot(loadFXML(fxml));
+    // }
+
+    // private static Parent loadFXML(String fxml) throws IOException {
+    //     FXMLLoader fxmlLoader = new FXMLLoader(sprint_0_GUI.class.getResource(fxml + ".fxml"));
+    //     return fxmlLoader.load();
+    // }
+
+    // @FXML
+    // private void switchToSecondary() throws IOException {
+    //     sprint_0_GUI.setRoot("secondary");
+    // }
+
+    // public class Square extends Pane {
+	// 	public Square() {
+	// 		setStyle("-fx-border-color: white");
+	// 		this.setPrefSize(2000, 2000);
+	// 		this.setOnMouseClicked(e -> handleMouseClick());
+	// 	}
+
+	// 	private void handleMouseClick() {
+	// 	}
+
+	// }
     public static void main(String[] args) {
         launch();
     }
