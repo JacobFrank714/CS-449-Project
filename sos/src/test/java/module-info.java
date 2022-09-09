fce2910c90a536fb3e19a69fab6fb9dec9177bf4
mcfreak12 <jacobfrank714@gmail.com>
2022-09-09 09:04:49 -0500

    no errors anymore currently

--- sos/src/test/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module sprint_0.app {
-    requires transitive javafx.controls;
-    requires transitive javafx.fxml;
-
-    opens sprint_0.app to javafx.fxml;
-    exports sprint_0.app;
-}
--- sos/src/test/java/sprint_0/test/sprint_0_GUI_Test.java
+++ sos/src/test/java/sprint_0/test/sprint_0_GUI_Test.java
@@ -1,5 +1,7 @@
 package sprint_0.test;
 
+// import org.junit.BeforeClass;
+
 public class sprint_0_GUI_Test {
     
 }
