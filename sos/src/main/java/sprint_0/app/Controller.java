4507dd9183194d8179616cf810d4d9b63e2474ca
mcfreak12 <jacobfrank714@gmail.com>
2022-09-12 10:46:48 -0500

    finished buttons. needs code and board procedures

---
+++ sos/src/main/java/sprint_0/app/Controller.java
@@ -0,0 +1,18 @@
+package sprint_0.app;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void changeGameType(ActionEvent e){
+        System.out.println("Change game type");
+    }
+
+    public void changePlayerType(ActionEvent e){
+        System.out.println("change player type");
+    }
+
+    public void changePlayerMove(ActionEvent e){
+        System.out.println("change player move");
+    }
+}
--- sos/src/main/java/sprint_0/app/GUI.java
+++ sos/src/main/java/sprint_0/app/GUI.java
@@ -1,9 +1,9 @@
 package sprint_0.app;
 
 import javafx.application.Application;
-// import javafx.fxml.FXML;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
@@ -11,74 +11,54 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
+import java.io.IOException;
+
+
 // import java.io.IOException;
 
 public class GUI extends Application{
 
-    private Square[][] squares;
-
     private Label gameStatus = new Label("Player 1's Turn");
+
+	private String fxml = "/resources/main";
+
+	private static Scene scene;
 
     static private Board board;
 
     @Override
-    public void start(Stage primaryStage) {
-		if (board == null) {
-			board = new Board(8);
-		}
-		GridPane pane = new GridPane();
-		squares = new Square[8][8];
-		for (int i = 0; i < 8; i++)
-			for (int j = 0; j < 8; j++)
-				pane.add(squares[i][j] = new Square(), j, i);
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML(fxml));
+        stage.setScene(scene);
+        stage.show();
+		// if (board == null) {
+		// 	board = new Board(8);
+		// }
+		// GridPane pane = new GridPane();
+		// squares = new Square[8][8];
+		// for (int i = 0; i < 8; i++)
+		// 	for (int j = 0; j < 8; j++)
+		// 		pane.add(squares[i][j] = new Square(), j, i);
 
-		BorderPane borderPane = new BorderPane();
-		borderPane.setCenter(pane);
-		borderPane.setBottom(gameStatus);
+		// BorderPane borderPane = new BorderPane();
+		// borderPane.setCenter(pane);
+		// borderPane.setBottom(gameStatus);
 
-		Scene scene = new Scene(borderPane, 800, 800);
-		primaryStage.setTitle("SOS Game");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		// Scene scene = new Scene(borderPane, 800, 800);
+		// primaryStage.setTitle("SOS Game");
+		// primaryStage.setScene(scene);
+		// primaryStage.show();
 	}
 
-    public class Square extends Pane {
-		public Square() {
-			setStyle("-fx-border-color: white");
-			this.setPrefSize(2000, 2000);
-			this.setOnMouseClicked(e -> handleMouseClick());
-		}
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
 
-		private void handleMouseClick() {
-		}
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
-	}
-
-    // static void setRoot(String fxml) throws IOException {
-    //     scene.setRoot(loadFXML(fxml));
-    // }
-
-    // private static Parent loadFXML(String fxml) throws IOException {
-    //     FXMLLoader fxmlLoader = new FXMLLoader(sprint_0_GUI.class.getResource(fxml + ".fxml"));
-    //     return fxmlLoader.load();
-    // }
-
-    // @FXML
-    // private void switchToSecondary() throws IOException {
-    //     sprint_0_GUI.setRoot("secondary");
-    // }
-
-    // public class Square extends Pane {
-	// 	public Square() {
-	// 		setStyle("-fx-border-color: white");
-	// 		this.setPrefSize(2000, 2000);
-	// 		this.setOnMouseClicked(e -> handleMouseClick());
-	// 	}
-
-	// 	private void handleMouseClick() {
-	// 	}
-
-	// }
     public static void main(String[] args) {
         launch();
     }
