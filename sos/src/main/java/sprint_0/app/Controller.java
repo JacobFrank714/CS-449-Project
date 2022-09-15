880f5c22bd5985fddd4a5fed76154ff40a521fce
mcfreak12 <jacobfrank714@gmail.com>
2022-09-15 14:04:13 -0500

    rearranging classes

--- sos/src/main/java/sprint_0/app/Controller.java
+++ sos/src/main/java/sprint_0/app/Controller.java
@@ -7,22 +7,25 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.scene.layout.BorderPane;
+import java.util.ArrayList;
+import javafx.scene.text.Font;
+import javafx.geometry.Pos;
+import sprint_0.app.Square;
+
+// import javafx.scene.control.;
 
 public class Controller {
 
     private Square[][] squares;
     
-    public Label gameType = new Label("SM");
-	public Label redPlayerType = new Label("CP");
-	public Label bluePlayerType = new Label("CP");
-	public Label redPlayerMove = new Label("S");
-	public Label bluePlayerMove = new Label("S");
+    private String gameType = new String("SM");
+
 
     @FXML
     private RadioButton gameTypeSM,gameTypeBL,bluePlayerTypeHP,bluePlayerTypeCP,redPlayerTypeHP,redPlayerTypeCP,bluePlayerMoveS,bluePlayerMoveO,redPlayerMoveS,redPlayerMoveO;
-    
     @FXML
     private TextField boardSize;
     @FXML
@@ -31,60 +34,51 @@ public class Controller {
     public void changeGameType(ActionEvent e){
         
         if (gameTypeSM.isSelected()){
-            gameType.setText("SM");
+            gameType="SM";
         }
         else if (gameTypeBL.isSelected()) {
-            gameType.setText("BL");
+            gameType = "BL";
         }
-        System.out.println(gameType.getText());
     }
     
     public void changeBluePlayerType(ActionEvent e){
         
         if (bluePlayerTypeHP.isSelected()){
-            bluePlayerType.setText("HP");
+            setBlueType("HP");
         }
         else if (bluePlayerTypeCP.isSelected()){
-            bluePlayerType.setText("CP");
+            setBlueType("CP");
         }
-        
-        System.out.println(bluePlayerType.getText());
     }
     
     public void changeRedPlayerType(ActionEvent e){
         
         if (redPlayerTypeHP.isSelected()){
-            redPlayerType.setText("HP");
+            setRedType("HP");
         }
         else if (redPlayerTypeCP.isSelected()){
-            redPlayerType.setText("CP");
+            setRedType("CP");
         }
-        
-        System.out.println(redPlayerType.getText());
     }
     
     public void changeBluePlayerMove(ActionEvent e){
         
         if (bluePlayerMoveO.isSelected()){
-            bluePlayerMove.setText("O");
+            bluePlayerMove="O";
         }
         else if (bluePlayerMoveS.isSelected()){
-            bluePlayerMove.setText("S");
+            bluePlayerMove="S";
         }
-        
-        System.out.println(bluePlayerMove.getText());
     }
     
     public void changeRedPlayerMove(ActionEvent e){
         
         if (redPlayerMoveO.isSelected()){
-            redPlayerMove.setText("O");
+            redPlayerMove="O";
         }
         else if (redPlayerMoveS.isSelected()){
-            redPlayerMove.setText("S");
+            redPlayerMove="S";
         }
-        
-        System.out.println(redPlayerMove.getText());
     }
     
     public void setBaordSize(ActionEvent e){
@@ -97,26 +91,11 @@ public class Controller {
         
         for(int i=0;i<x; i++){
             for(int j=0;j<x; j++){
-                gridBoard.add(squares[i][j] = new Square(), j, i);
+                gridBoard.add(squares[i][j] = new Square(i, j), j, i);
             }
         }
         
         borderPane.setCenter(gridBoard);
     }
     
-    public class Square extends Pane {
-        public Square() {
-            setStyle("-fx-border-color: white; -fx-background-color: lightgrey;");
-			this.setPrefSize(2000, 2000);
-			this.setOnMouseClicked(e -> handleMouseClick());
-		}
-        
-		private void handleMouseClick() {
-            System.out.println("click");
-		}
-
-	}
-
-    public class makeMove {
-	}
 }
