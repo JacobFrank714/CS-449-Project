1c47c33eb07d0ce9353a31533aee1bedaeaabd3b
mcfreak12 <jmft87@mail.umkc.edu>
2022-09-16 18:52:27 -0500

    random

--- sos/src/main/java/sprint_0/app/Square.java
+++ sos/src/main/java/sprint_0/app/Square.java
@@ -8,6 +8,7 @@ public class Square extends StackPane {
 	public Square(int x, int y){
 		setStyle("-fx-border-color: white; -fx-background-color: lightgrey;");
 		setId(String.valueOf(x)+"," +String.valueOf(y));
+		this.setPrefSize(2000, 2000);
 		this.setOnMouseClicked(e -> handleMouseClick());
         Game.spaces.add(this);
 	}
